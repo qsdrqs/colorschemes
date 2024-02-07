700e4a1f5efac792f6ce015a50aa07bee77f7b6f
qsdrqs <qsdrqs@gmail.com>
2024-02-07 00:19:22 -0600

    modify colorscheme

--- zenburn.py
+++ zenburn.py
@@ -5,15 +5,30 @@
 # Copyright (C) 2009-2013  Roman Zimbelmann <hut@lepus.uberspace.de>
 # This software is distributed under the terms of the GNU GPL version 3.
 
+# Copied from https://github.com/ranger/colorschemes/blob/master/zenburn.py
+# Under the terms of the GNU GPL version 3.
+
+'''
+Last modified on 12/30/2020
+
+@author qsdrqs
+'''
+
+
+from __future__ import (absolute_import, division, print_function)
+
 from ranger.gui.colorscheme import ColorScheme
-from ranger.gui.color import default_colors, reverse, bold, normal, default
+from ranger.gui.color import (
+    black, blue, cyan, green, magenta, red, white, yellow, default,
+    normal, bold, reverse, dim, BRIGHT,
+    default_colors,
+)
 
 
-# pylint: disable=too-many-branches,too-many-statements
-class Zenburn(ColorScheme):
-    progress_bar_color = 108
+class Default(ColorScheme):
+    progress_bar_color = blue
 
-    def use(self, context):
+    def use(self, context):  # pylint: disable=too-many-branches,too-many-statements
         fg, bg, attr = default_colors
 
         if context.reset:
@@ -25,94 +40,121 @@ class Zenburn(ColorScheme):
             else:
                 attr = normal
             if context.empty or context.error:
-                fg = 174
-                bg = 235
+                bg = red
             if context.border:
-                fg = 248
-            if context.image:
-                fg = 109
-            if context.video:
-                fg = 66
-            if context.audio:
-                fg = 116
-            if context.document:
-                fg = 151
+                fg = default
+            if context.media:
+                if context.image:
+                    fg = yellow
+                else:
+                    fg = magenta
             if context.container:
-                attr |= bold
-                fg = 174
+                fg = 127
             if context.directory:
                 attr |= bold
-                fg = 223
+                fg = blue
+                fg += BRIGHT
             elif context.executable and not \
                     any((context.media, context.container,
                          context.fifo, context.socket)):
                 attr |= bold
-                fg = 108
+                fg = green
+                fg += BRIGHT
             if context.socket:
-                fg = 180
                 attr |= bold
+                fg = magenta
+                fg += BRIGHT
             if context.fifo or context.device:
-                fg = 144
+                fg = yellow
                 if context.device:
                     attr |= bold
+                    fg += BRIGHT
+            if context.pdf:
+                fg = red
+            if context.msdoc:
+                fg = 27
+            if context.msppt:
+                fg = 208
+            if context.msxls:
+                fg = 28
             if context.link:
-                fg = 223 if context.good else 116
-                bg = 234
-            if context.bad:
-                bg = 235
+                fg = cyan if context.good else magenta
+            if context.apk:
+                fg = 34
             if context.tag_marker and not context.selected:
                 attr |= bold
-                if fg in (174, 95):
-                    fg = 248
+                if fg in (red, magenta):
+                    fg = white
                 else:
-                    fg = 174
+                   fg = red
+                fg += BRIGHT
             if not context.selected and (context.cut or context.copied):
-                fg = 108
-                bg = 234
+                attr |= bold
+                fg = black
+                fg += BRIGHT
+                # If the terminal doesn't support bright colors, use dim white
+                # instead of black.
+                if BRIGHT == 0:
+                    attr |= dim
+                    fg = white
             if context.main_column:
+                # Doubling up with BRIGHT here causes issues because it's
+                # additive not idempotent.
                 if context.selected:
                     attr |= bold
                 if context.marked:
                     attr |= bold
-                    fg = 223
+                    fg = yellow
             if context.badinfo:
                 if attr & reverse:
-                    bg = 95
+                    bg = magenta
                 else:
-                    fg = 95
+                    fg = magenta
+
+            if context.inactive_pane:
+                fg = cyan
 
         elif context.in_titlebar:
-            attr |= bold
             if context.hostname:
-                fg = 174 if context.bad else 180
+                fg = red if context.bad else green
             elif context.directory:
-                fg = 223
+                fg = blue
             elif context.tab:
                 if context.good:
-                    bg = 180
+                    bg = green
             elif context.link:
-                fg = 116
+                fg = cyan
+            attr |= bold
 
         elif context.in_statusbar:
             if context.permissions:
                 if context.good:
-                    fg = 108
+                    fg = cyan
                 elif context.bad:
-                    fg = 174
+                    fg = magenta
             if context.marked:
                 attr |= bold | reverse
-                fg = 223
+                fg = yellow
+                fg += BRIGHT
+            if context.frozen:
+                attr |= bold | reverse
+                fg = cyan
+                fg += BRIGHT
             if context.message:
                 if context.bad:
                     attr |= bold
-                    fg = 174
+                    fg = red
+                    fg += BRIGHT
             if context.loaded:
                 bg = self.progress_bar_color
             if context.vcsinfo:
-                fg = 116
+                fg = blue
                 attr &= ~bold
             if context.vcscommit:
-                fg = 144
+                fg = yellow
+                attr &= ~bold
+            if context.vcsdate:
+                fg = cyan
                 attr &= ~bold
 
         if context.text:
@@ -121,7 +163,7 @@ class Zenburn(ColorScheme):
 
         if context.in_taskview:
             if context.title:
-                fg = 116
+                fg = blue
 
             if context.selected:
                 attr |= reverse
@@ -135,29 +177,31 @@ class Zenburn(ColorScheme):
         if context.vcsfile and not context.selected:
             attr &= ~bold
             if context.vcsconflict:
-                fg = 95
+                fg = magenta
+            elif context.vcsuntracked:
+                fg = cyan
             elif context.vcschanged:
-                fg = 174
+                fg = red
             elif context.vcsunknown:
-                fg = 174
+                fg = red
             elif context.vcsstaged:
-                fg = 108
+                fg = green
             elif context.vcssync:
-                fg = 108
+                fg = green
             elif context.vcsignored:
                 fg = default
 
         elif context.vcsremote and not context.selected:
             attr &= ~bold
-            if context.vcssync:
-                fg = 108
+            if context.vcssync or context.vcsnone:
+                fg = green
             elif context.vcsbehind:
-                fg = 174
+                fg = red
             elif context.vcsahead:
-                fg = 116
+                fg = blue
             elif context.vcsdiverged:
-                fg = 95
+                fg = magenta
             elif context.vcsunknown:
-                fg = 174
+                fg = red
 
         return fg, bg, attr
